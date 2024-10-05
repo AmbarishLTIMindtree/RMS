061442bd1eeeed9b75b95ca8af6203c633711e75
AmbarishLTIMindtree <ambarish8653@gmail.com>
2024-10-05 18:29:52 +0530

    forEachComponent updated.

---
+++ force-app/main/default/lwc/forEachComponent/forEachComponent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachComponent extends LightningElement {
+
+    listOfRecords = ['Ambarish', 'Akash','Chetan','Preeti','Nikshita'];
+}
