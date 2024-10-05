ac0e52f5d8ed153c424cd3487e8ae37e3eecc150
AmbarishLTIMindtree <ambarish8653@gmail.com>
2024-10-05 17:50:04 +0530

    updated

---
+++ force-app/main/default/lwc/dataBindingComponent/dataBindingComponent.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+// Data Binding : The process of tranfering the data or display data from JS file to HTML file.
+// Property : Variable in JS file.
+
+export default class DataBindingComponent extends LightningElement {
+
+    firstName = 'Akash';
+
+}
---
+++ force-app/main/default/lwc/sldsExampleComp/sldsExampleComp.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+// SLDS : Salesforce Lightning Design System (https://www.lightningdesignsystem.com/)
+
+export default class SldsExampleComp extends LightningElement {}
